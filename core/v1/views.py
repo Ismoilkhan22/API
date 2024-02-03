d64f920fd24295a66d94124d837212abe19da8c4
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2024-02-03 17:25:26 +0500

    api2

--- core/v1/views.py
+++ core/v1/views.py
@@ -1,6 +1,9 @@
+from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 from core.models import Category, Product
 from core.serializer import CtgSerializer, ProductSerializer
 
@@ -13,4 +16,18 @@ class CategoryView(viewsets.ModelViewSet):
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        product = self.get_object()
+        user = self.request.user
+
+        if user in product.likes.all():
+            product.likes.remove(user)
+            liked = False
+        else:
+            product.likes.add(user)
+            liked = True
+
+        return Response({'liked': liked})
 
